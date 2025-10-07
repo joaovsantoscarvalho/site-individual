52753469001585b6276b8782d4ca72dbc3e93cc4
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-10-07 09:02:09 -0300

    .

--- main.js
+++ main.js
@@ -4,4 +4,6 @@ const interactiveText = document.getElementById("interactive-text");
 button.addEventListener('click', () => {
     const corAleatoria = '#' + Math.floor(Math.random() *16777215).toString(16);
     document.body.style.backgroundColor = corAleatoria;
+
+    interactiveText.textContent = 'Cor de fundo alterada para ${corAleatoria}'
 });
