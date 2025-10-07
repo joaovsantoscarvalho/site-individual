f2a6b1b920aa7cc6a64539fcedc0e7117e874b4f
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-10-07 09:20:37 -0300

    .

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ button.addEventListener('click', () => {
     const corAleatoria = '#' + Math.floor(Math.random() *16777215).toString(16);
     document.body.style.backgroundColor = corAleatoria;
 
-    interactiveText.textContent = 'Cor de fundo alterada para ${corAleatoria}'
+    interactiveText.textContent = `Cor de fundo alterada para ${corAleatoria}`;
 });
