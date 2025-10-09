86c722703e8b5076be54fd4af48bd0f8755d3961
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-10-09 10:36:34 -0300

    .

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-const button = document.querySelector("button");
-const interactiveText = document.getElementById("interactive-text");
-
-button.addEventListener('click', () => {
-    const corAleatoria = '#' + Math.floor(Math.random() *16777215).toString(16);
-    document.body.style.backgroundColor = corAleatoria;
-
-    interactiveText.textContent = `Cor de fundo alterada para ${corAleatoria}`;
+document.getElementById('contactButton').addEventListener('click' function() {
+    alert();
 });
