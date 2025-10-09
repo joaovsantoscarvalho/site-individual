e498bc464b2539c7f5bc1037441a344677bdc496
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-10-09 10:38:13 -0300

    .

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-document.getElementById('contactButton').addEventListener('click' function() {
-    alert();
+document.getElementById('contactButton').addEventListener('click', function() {
+    alert('sua mensagem foi enviada com sucessso!');
 });
